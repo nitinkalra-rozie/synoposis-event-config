e72fb72fc0e89d3724fde1b016a212199c05042b
Shreyash Rozie <shreyas@rozie.ai>
2024-05-02 14:08:38 +0530

    api integrated for putting transcript on dynamodb

--- src/app/services/backend-api.service.ts
+++ src/app/services/backend-api.service.ts
@@ -11,4 +11,12 @@ export class BackendApiService {
   getTranscriberPreSignedUrl(body:any){
     return this.http.post('https://fqbvo8ifm0.execute-api.ca-central-1.amazonaws.com/dev/getPreSignedUrl', body);
   }
+
+  putTranscript(transcript:any){
+    const body = {
+      sessionId:"test",
+      Transcript: transcript.Transcript
+    }
+    return this.http.post('https://xmvv5lp42m.execute-api.ca-central-1.amazonaws.com/dev/postTranscript', body);
+  }
 }
