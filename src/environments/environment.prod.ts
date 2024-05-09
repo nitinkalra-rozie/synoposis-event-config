5b4dff6714f3d5985f3948367c9de0004bcf8f30
rushikeshekashinge <rushikesh@rozie.ai>
2024-05-09 14:51:02 +0530

    added env for prod and dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  cognitoUserPoolId:'ca-central-1_V9KhUwxNc',
+  cognitoAppClientId:'3ppt7i0sou74e5bks8mc5ph350',
+  getTranscriberPreSignedUrl:'',
+  putTranscript:'',
+  postData:'',
+  getEventDetails:'',
+  postCurrentSessionId:''
 };
