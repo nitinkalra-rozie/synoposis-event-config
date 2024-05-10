26e5c97e2df5a69ec81d4e241624ec0082b5af35
Shreyash Rozie <shreyas@rozie.ai>
2024-05-11 02:15:41 +0530

    prod env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   cognitoUserPoolId:'ca-central-1_V9KhUwxNc',
   cognitoAppClientId:'3ppt7i0sou74e5bks8mc5ph350',
-  getTranscriberPreSignedUrl:'',
-  putTranscript:'',
-  postData:'',
-  getEventDetails:'',
-  postCurrentSessionId:''
+  getTranscriberPreSignedUrl:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/getPreSignedUrl',
+  putTranscript:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/postTranscript',
+  postData:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/adminapplambdaconfig',
+  getEventDetails:'https://ebnyn5b8bh.execute-api.ca-central-1.amazonaws.com/prod/getEventDetails',
+  postCurrentSessionId:'https://w4cipy0yrk.execute-api.ca-central-1.amazonaws.com/prod/getCurrentSessionDetails'
 };
