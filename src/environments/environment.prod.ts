2e9593ffe9d551a5bf3580775d7ef409cbd91793
shashankmali <shashank@rozie.ai>
2024-05-14 01:20:55 +0530

    prod env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  cognitoUserPoolId:'ca-central-1_sBbyiaKRm',
-  cognitoAppClientId:'tkn7hpp0pd80vd70h28sl1qt3',
-  getTranscriberPreSignedUrl:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/getPreSignedUrl',
-  putTranscript:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/postTranscript',
-  postData:'https://mwel9x3bkb.execute-api.ca-central-1.amazonaws.com/prod/adminapplambdaconfig',
-  getEventDetails:'https://ebnyn5b8bh.execute-api.ca-central-1.amazonaws.com/prod/getEventDetails',
-  postCurrentSessionId:'https://w4cipy0yrk.execute-api.ca-central-1.amazonaws.com/prod/getCurrentSessionDetails'
+  cognitoUserPoolId:'ca-central-1_NSVSNiKc4',
+  cognitoAppClientId:'2qjj2dg8hm9m6heeiulemddbff',
+  getTranscriberPreSignedUrl:'https://ly3c14q59d.execute-api.ca-central-1.amazonaws.com/prod/getPreSignedUrl',
+  putTranscript:'https://ly3c14q59d.execute-api.ca-central-1.amazonaws.com/prod/postTranscript',
+  postData:'https://ly3c14q59d.execute-api.ca-central-1.amazonaws.com/prod/adminapplambdaconfig',
+  getEventDetails:'https://7stlmv62vk.execute-api.ca-central-1.amazonaws.com/prod/getEventDetails',
+  postCurrentSessionId:'https://yg4vlhyu1g.execute-api.ca-central-1.amazonaws.com/prod/getCurrentSessionDetails'
 };
