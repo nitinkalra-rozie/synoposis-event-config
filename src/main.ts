46272b0a4a9142cc642111f0b9a34816e8c067d0
Siddhesh1045 <siddhesh@rozie.ai>
2024-07-23 01:36:43 +0530

    added wablock

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,13 @@ if (environment.production) {
   }
   console.debug = console.error = () => { };
 }
+tryKeepScreenAlive(1000000);
+function tryKeepScreenAlive(minutes) {
+  navigator.wakeLock.request("screen").then(lock => {
+    setTimeout(() => lock.release(), minutes * 60 * 1000);
+  });
+}
 
+tryKeepScreenAlive(10);
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
