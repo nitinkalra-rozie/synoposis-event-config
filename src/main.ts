7fc4f3624d83b4ad436f46b1d3e64ae46c7f4222
saiyaffrozie <saiyaff@rozie.ai>
2024-09-12 15:32:08 +0530

    disable image related warnings since NgOptimizedImage

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { environment } from './environments/environment';
 import NoSleep from '@uriopass/nosleep.js';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { CommonModule, IMAGE_CONFIG } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AuthService } from './app/services/auth.service';
@@ -16,31 +16,41 @@ if (environment.production) {
 }
 if (environment.production) {
   if (window) {
-      window.console.debug = window.console.error = () => { };
+    window.console.debug = window.console.error = () => {};
   }
-  console.debug = console.error = () => { };
+  console.debug = console.error = () => {};
 }
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(AppRoutingModule, FormsModule, CommonModule, BrowserModule, ReactiveFormsModule),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptorService,
-            multi: true,
-        },
-        AuthApiService,
-        AuthService,
-        provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch(err => console.log(err));
+  providers: [
+    importProvidersFrom(AppRoutingModule, FormsModule, CommonModule, BrowserModule, ReactiveFormsModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+    AuthApiService,
+    AuthService,
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        disableImageSizeWarning: true,
+        disableImageLazyLoadWarning: true,
+      },
+    },
+  ],
+}).catch(err => console.log(err));
 
 var wakeLockEnabled = false;
-var toggleEl = document.querySelector("#index-body");
-toggleEl.addEventListener('click', function() {
-  if (!wakeLockEnabled) {
-    noSleep.enable();
-    wakeLockEnabled = true;
-  } else {
-  }
-}, false);
+var toggleEl = document.querySelector('#index-body');
+toggleEl.addEventListener(
+  'click',
+  function () {
+    if (!wakeLockEnabled) {
+      noSleep.enable();
+      wakeLockEnabled = true;
+    } else {
+    }
+  },
+  false
+);
