94c7deae986b9462ef94808388cf760951a207b4
Naveen Cooray <naveen.cooray.nc@gmail.com>
2024-09-23 12:24:19 +0530

    Refactor sidebar and add info icon to topbar

--- src/app/components/shared/side-bar/side-bar.component.ts
+++ src/app/components/shared/side-bar/side-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss'],
+  imports: [RouterModule, MatTooltipModule, MatIconModule],
   standalone: true,
 })
 export class SideBarComponent {}
