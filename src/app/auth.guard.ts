3e9eef965933905c6def8250ce06f70b46dffdd5
saiyaffrozie <saiyaff@rozie.ai>
2024-11-28 18:01:22 +0530

    Add guard for the admin route to prevent user from the direct access

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -11,22 +11,33 @@ export class AuthGuard {
     private router: Router
   ) {}
 
-  canActivate(): Promise<boolean> {
-    return new Promise<boolean>((resolve) => {
-      this.authService
-        .checkSession()
-        .then(() => {
-          if (this.authService.isAuthenticated()) {
-            resolve(true);
-          } else {
-            this.router.navigate(['/']);
-            resolve(false);
-          }
-        })
-        .catch(() => {
-          this.router.navigate(['/']);
-          resolve(false);
-        });
-    });
+  canActivate(): boolean {
+    // This is a temporary implementation of checking if the user is authenticated. Check TODO below
+    if (this.authService.getAccessToken()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
+
+  // TODO: @later - Implement a proper login flow to use cognito or any auth provider and check session
+  // canActivate(): Promise<boolean> {
+  //   return new Promise<boolean>((resolve) => {
+  //     this.authService
+  //       .checkSession()
+  //       .then(() => {
+  //         if (this.authService.isAuthenticated()) {
+  //           resolve(true);
+  //         } else {
+  //           this.router.navigate(['/']);
+  //           resolve(false);
+  //         }
+  //       })
+  //       .catch(() => {
+  //         this.router.navigate(['/']);
+  //         resolve(false);
+  //       });
+  //   });
+  // }
 }
