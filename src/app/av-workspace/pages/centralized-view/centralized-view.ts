4dd09cb3d9e97490d719fd74ebbc0d2cc9329aa5
saiyaffrozie <saiyaff@rozie.ai>
2025-06-05 08:15:32 +0530

    feat: Add header section, search input, and filter with multi-select component

--- src/app/av-workspace/pages/centralized-view/centralized-view.ts
+++ src/app/av-workspace/pages/centralized-view/centralized-view.ts
@@ -1,10 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { SynMultiSelect } from 'src/app/shared/components/syn-multi-select/syn-multi-select';
+import { SynSearchInput } from '../../../shared/components/syn-search-input/syn-search-input';
 
 @Component({
   selector: 'app-centralized-view',
   templateUrl: './centralized-view.html',
   styleUrl: './centralized-view.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    SynMultiSelect,
+    SynSearchInput,
+  ],
 })
-export class CentralizedView {}
+export class CentralizedView {
+  protected isLoading = signal(false);
+  protected productSearchTerm = signal('');
+
+  protected onSearch(value: string): void {
+    console.log(value);
+  }
+
+  protected executeSearch(value: string): void {
+    console.log(value);
+  }
+}
