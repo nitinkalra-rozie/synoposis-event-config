e837b634d34ec13f83e15de54e561c336e592a7b
saiyaffrozie <saiyaff@rozie.ai>
2025-06-07 02:45:38 +0530

    perf: Simplify syn-multi-select component by removing uiConfig and update label handling for better performance

--- src/app/shared/components/syn-multi-select/syn-multi-select-option.model.ts
+++ src/app/shared/components/syn-multi-select/syn-multi-select-option.model.ts
@@ -4,9 +4,3 @@ export interface SynMultiSelectOption<T> {
   children?: SynMultiSelectOption<T>[];
   elementClass?: string;
 }
-
-export interface SynMultiSelectConfig {
-  applyFilterButtonText?: string;
-  labelSearch?: string;
-  labelPosition?: 'before' | 'after';
-}
