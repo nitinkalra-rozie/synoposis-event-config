87fa90fd7c8a14c094d1201bc8e25d53d81148e9
saiyaffrozie <saiyaff@rozie.ai>
2025-08-19 18:04:19 +0530

    feat: Enhance app event configuration initializer to handle errors with toast notifications

--- src/app/core/config/app-event-config-init.ts
+++ src/app/core/config/app-event-config-init.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { firstValueFrom, map } from 'rxjs';
+import { catchError, firstValueFrom, map, throwError } from 'rxjs';
 import { EventConfigResponseData } from 'src/app/core/data-models/event-config.data-model';
 import { EventConfigStore } from 'src/app/core/stores/event-config-store';
+import { SynToastFacade } from 'src/app/shared/components/syn-toast/syn-toast-facade';
 import { getEventDomain } from 'src/app/shared/utils/get-event-domain-util';
 import { environment } from 'src/environments/environment';
 
@@ -10,6 +11,7 @@ export function appEventConfigInitializer() {
   const http = inject(HttpClient);
   const apiUrl = environment.apiBaseUrl;
   const eventConfigStore = inject(EventConfigStore);
+  const toastFacade = inject(SynToastFacade);
 
   return (): Promise<void> =>
     firstValueFrom(
@@ -21,6 +23,10 @@ export function appEventConfigInitializer() {
           map((response) => {
             eventConfigStore.setEventIdentifier(response.data.EventIdentifier);
             eventConfigStore.setEventConfigData(response.data);
+          }),
+          catchError((error) => {
+            toastFacade.showError('Error fetching event config');
+            return throwError(() => error);
           })
         )
     );
